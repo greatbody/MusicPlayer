26969b739d5f6f3e1d8631d5994127ad97a17cbf
greatbody <wedown@163.com>
2015-05-17 22:52:35 +0800

    init project

---
+++ MusicPlayer/Player.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Player
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.btnInitAuto = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.btnChooseFile = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblPosition = New System.Windows.Forms.Label()
+        Me.lblNowTime = New System.Windows.Forms.Label()
+        Me.lblNow = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.DateTimePicker1.Location = New System.Drawing.Point(96, 7)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(134, 21)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'btnInitAuto
+        '
+        Me.btnInitAuto.Location = New System.Drawing.Point(236, 5)
+        Me.btnInitAuto.Name = "btnInitAuto"
+        Me.btnInitAuto.Size = New System.Drawing.Size(68, 26)
+        Me.btnInitAuto.TabIndex = 1
+        Me.btnInitAuto.Text = "启动"
+        Me.btnInitAuto.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.HideSelection = False
+        Me.txtFilePath.Location = New System.Drawing.Point(15, 73)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.ReadOnly = True
+        Me.txtFilePath.Size = New System.Drawing.Size(493, 21)
+        Me.txtFilePath.TabIndex = 2
+        '
+        'btnChooseFile
+        '
+        Me.btnChooseFile.Location = New System.Drawing.Point(448, 5)
+        Me.btnChooseFile.Name = "btnChooseFile"
+        Me.btnChooseFile.Size = New System.Drawing.Size(64, 26)
+        Me.btnChooseFile.TabIndex = 3
+        Me.btnChooseFile.Text = "选择文件"
+        Me.btnChooseFile.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 103)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(493, 25)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "播放结束时间"
+        '
+        'lblPosition
+        '
+        Me.lblPosition.AutoSize = True
+        Me.lblPosition.Location = New System.Drawing.Point(328, 16)
+        Me.lblPosition.Name = "lblPosition"
+        Me.lblPosition.Size = New System.Drawing.Size(53, 12)
+        Me.lblPosition.TabIndex = 7
+        Me.lblPosition.Text = "--:--:--"
+        '
+        'lblNowTime
+        '
+        Me.lblNowTime.AutoSize = True
+        Me.lblNowTime.Location = New System.Drawing.Point(13, 45)
+        Me.lblNowTime.Name = "lblNowTime"
+        Me.lblNowTime.Size = New System.Drawing.Size(53, 12)
+        Me.lblNowTime.TabIndex = 8
+        Me.lblNowTime.Text = "当前时间"
+        '
+        'lblNow
+        '
+        Me.lblNow.AutoSize = True
+        Me.lblNow.Location = New System.Drawing.Point(72, 45)
+        Me.lblNow.Name = "lblNow"
+        Me.lblNow.Size = New System.Drawing.Size(0, 12)
+        Me.lblNow.TabIndex = 9
+        '
+        'Player
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 140)
+        Me.Controls.Add(Me.lblNow)
+        Me.Controls.Add(Me.lblNowTime)
+        Me.Controls.Add(Me.lblPosition)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnChooseFile)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.btnInitAuto)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Player"
+        Me.Text = "音乐播放器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnInitAuto As System.Windows.Forms.Button
+    Friend WithEvents txtFilePath As System.Windows.Forms.TextBox
+    Friend WithEvents btnChooseFile As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblPosition As System.Windows.Forms.Label
+    Friend WithEvents lblNowTime As System.Windows.Forms.Label
+    Friend WithEvents lblNow As System.Windows.Forms.Label
+
+End Class
---
+++ MusicPlayer/Player.vb
@@ -0,0 +1,146 @@
+﻿Imports SunSoftUtility
+Imports System.Threading
+
+Public Class Player
+    Dim _timedPlayer As Audio
+    Private Const PosInterver = 100
+    Private _mPlayThread As Thread
+    Private _bKeepPlay As Boolean = True
+#Region "控件事件代码"
+    ''' <summary>
+    ''' 启动、重置按钮控制
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnInitAuto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInitAuto.Click
+        Dim obj As New Object
+        If btnInitAuto.Text = "启动" Then
+            If txtFilePath.Text.Length > 0 Then
+                _timedPlayer = New Audio(txtFilePath.Text)
+                '设置进度条
+                With ProgressBar1
+                    .Minimum = 0
+                    .Maximum = CInt(_timedPlayer.Length / 1000)
+                End With
+                If Not _mPlayThread Is Nothing AndAlso _mPlayThread.ThreadState <> ThreadState.Aborted Then
+                    _mPlayThread.Abort()
+                End If
+                _mPlayThread = New Thread(AddressOf AutoPlay)
+                _mPlayThread.Start(DateTimePicker1.Value)
+                btnInitAuto.Text = "重置"
+                btnChooseFile.Enabled = False
+                SyncLock obj
+                    _bKeepPlay = True
+                End SyncLock
+
+            Else
+                MsgBox("请选择音乐文件！")
+                Exit Sub
+            End If
+        Else
+
+            SyncLock obj
+                _bKeepPlay = False
+            End SyncLock
+            btnInitAuto.Text = "启动"
+            lblPosition.Text = "--:--:--"
+            ProgressBar1.Value = 0
+            '文件选择解锁
+            btnChooseFile.Enabled = True
+        End If
+
+    End Sub
+    ''' <summary>
+    ''' 选择文件按钮控制
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnChooseFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnChooseFile.Click
+        txtFilePath.Text = FileSystem.SelectFiles("请选择一个音乐文件")
+    End Sub
+    ''' <summary>
+    ''' 窗口关闭前操作
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Player_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+    ''' <summary>
+    ''' 窗口初始化操作
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Player_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Me.Show()
+        Call (New Thread(AddressOf UpdateTimeDisplay)).Start()
+    End Sub
+#End Region
+
+#Region "线程函数"
+    Private Sub AutoPlay(ByVal BeginTime As Date)
+        Dim i As Integer = 0
+        Dim volume As Integer = 0
+
+        BeginTime = BeginTime.AddSeconds(_timedPlayer.Length / 1000 * -1)
+        While True
+            Application.DoEvents()
+            Thread.Sleep(PosInterver)
+            Select Case i
+                Case 0
+                    If BeginTime < Now Then
+                        _timedPlayer.Play()
+                        _timedPlayer.Volume = 0
+                        i = 1
+                    End If
+                Case 1
+                    If volume < 1000 Then
+                        volume += CInt(PosInterver / 8)
+                        _timedPlayer.Volume = volume
+                    Else
+                        i = 2
+                        Console.WriteLine("log finish")
+                    End If
+            End Select
+            Console.WriteLine(_bKeepPlay)
+            If _bKeepPlay = False Then
+                _timedPlayer.Pause()
+                Exit Sub
+            End If
+            Invoke(New VSetProgress(AddressOf SetProgress), CInt(_timedPlayer.Position / 1000))
+            Invoke(New VSetPositionString(AddressOf SetPositionString), _timedPlayer.CurrentPos)
+        End While
+    End Sub
+
+    Private Sub UpdateTimeDisplay()
+        While True
+            Invoke(New VSetPositionString(AddressOf SetTimeString), Format(Now, "yyyy-MM-dd HH:mm:ss"))
+            Application.DoEvents()
+            Thread.Sleep(250)
+        End While
+
+    End Sub
+#End Region
+
+#Region "委托-窗体控件操作"
+    Delegate Sub VSetProgress(ByVal Value As Integer)
+    Public Sub SetProgress(ByVal Value As Integer)
+        ProgressBar1.Value = Value
+        Console.WriteLine(Value)
+    End Sub
+
+    Delegate Sub VSetPositionString(ByVal s As String)
+    Public Sub SetPositionString(ByVal Position As String)
+        lblPosition.Text = Position
+    End Sub
+    Public Sub SetTimeString(ByVal CurTime As String)
+        lblNow.Text = CurTime
+    End Sub
+#End Region
+
+
+End Class
